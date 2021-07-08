05e1ac9453c2b66e973b164588e87726d0c7eaa6
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-07-08 15:13:45 +0200

    Create new class responsible for info about total time spent
    It is just a copy from GetStaticInfo, I want to separete it into
    dedicated file. There is one more difference, getTotalTime method does
    not return the info about total time, but integrate it with DOM
    immediately, before this method returns these info, cuz these
    informations were kept in special object, and there was a special method
    which was responsible for showing it, now there is no such a method and
    object so this method directly works with DOM

--- src/modules/index.js
+++ src/modules/index.js
@@ -6,6 +6,7 @@ import GetInfo from "./classes/GetStaticInfo";
 import BatteryInfo from "./classes/BatteryInfo";
 import NetworkStatus from "./classes/NetworkStatusInfo";
 import CursorMoveCoordinates from "./classes/CursorMoveCoordinatesInfo";
+import TotalTime from "./classes/TotalTimeSpentInfo";
 
 //variables ralated with DOM
 const infoContainer = document.querySelector("#infoContainer");
@@ -15,3 +16,4 @@ const info = new GetInfo(infoContainer);
 const batteryInfo = new BatteryInfo(infoContainer);
 const networkStatus = new NetworkStatus(infoContainer);
 const cursorMoveCoordinates = new CursorMoveCoordinates(infoContainer);
+const totalTime = new TotalTime(infoContainer);

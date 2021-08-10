2dcc1880bb72638d8b03480741185d24e6ffd19c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-10 17:51:43 +0200

    Showing the info about availability of a given media
    There are two method
    -renderMedia takes a "mediaType" parametr and checks if there is such a
    media and shows information about it
    -showMedia it is just a loop throught all of media types which are
    defined in index.js, this method calls renderMedia

--- src/modules/classes/UserMedia.js
+++ src/modules/classes/UserMedia.js
@@ -4,9 +4,33 @@ class UserMedia{
         this.infoContainer = infoContainer;
         //array of mediaTypes to check(media types are defined when the instance is created(index.js))
         this.meidaTypesToCheck = meidaTypesToCheck;
-        this.getMedia();
+        this.showMedia();
     }
+    //Loop throught all af media types which are defined in index.js
+    showMedia(){
+        for(const media of this.meidaTypesToCheck){
+            //Calling the method which renders informations about avaiable/unavaiable of media
+            this.renderMedia(media);
+        }
+    }
+    //Handling of the async method
+    renderMedia(mediaType){
+        this.getMedia().then((devices)=>{
+            //Check if getMedia() method returnes desired object(device)
+            const isMedia = devices.some(device => mediaType === device.kind);
 
+            //Creating HTML content for such an object(device), before if statment, cuz in if decides only about content of li(newInfo)
+            const newInfo = document.createElement("li");
+            newInfo.className = "basicList__element informations__info";
+            this.infoContainer.appendChild(newInfo);
+
+            if(isMedia){
+                newInfo.innerHTML = `${mediaType}: true`;
+            }else{
+                newInfo.innerHTML = `${mediaType}: false`;
+            }
+        });
+    }
     //async method which returnes info about avaiable media, it returnes array of objects which describle avaiavle media
     async getMedia(){
         try{

a7c09acb25fd4d19754761b15a75f66591da8020
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-08-10 17:27:19 +0200

    Write async method
    This method provides informations about avaiable devices(webcam, mic,
    etc). Now it logs array of objects with informations about devices

--- src/modules/classes/UserMedia.js
+++ src/modules/classes/UserMedia.js
@@ -4,8 +4,23 @@ class UserMedia{
         this.infoContainer = infoContainer;
         //array of mediaTypes to check(media types are defined when the instance is created(index.js))
         this.meidaTypesToCheck = meidaTypesToCheck;
+        this.getMedia();
     }
-
+    //async method which returnes info about avaiable media
+    async getMedia(){
+        try{
+            const md = navigator.mediaDevices;
+            if (!md || !md.enumerateDevices){
+              return false;
+            }else{
+                const devices = await md.enumerateDevices();
+                console.log(devices);
+                return devices;
+            }
+        }catch(err){
+            throw new Error(err);
+        }
+    }
 }
 
 export default UserMedia;

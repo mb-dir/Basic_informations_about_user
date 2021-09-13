986b0c109f8762befbba3e247a92bac5ce92d4f4
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-09-13 18:05:02 +0200

    Create auxiliary functions(callbacks) for geoloaction
    The navigator.geolocation.getCurrentPosition() function has two
    parameters(callbacks), one for success and one for error -
    https://developer.mozilla.org/pl/docs/Web/API/Geolocation_API

--- src/modules/classes/GeoCoordinates.js
+++ src/modules/classes/GeoCoordinates.js
@@ -12,6 +12,18 @@ class GeoCoordinates{
         newInfo.className = "basicList__element informations__info";
 
         this.infoContainer.appendChild(newInfo);
+
+        //auxiliary functions related with geolocation API(https://developer.mozilla.org/pl/docs/Web/API/Geolocation_API)- these are navigator.geolocation.getCurrentPosition callbacks
+        function success(position) {
+            const latitude  = position.coords.latitude;
+            const longitude = position.coords.longitude;
+            
+            newInfo.innerHTML = `latitude/longitude: ${latitude} ${longitude}`;
+          }
+        
+          function error() {
+            newInfo.innerHTML = "Unable to retrieve your location";
+          }
     }
 }
 

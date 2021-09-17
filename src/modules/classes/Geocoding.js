08408c8b1534059fd9fc9756403516fbb5d16c23
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-09-17 20:25:52 +0200

    Create basic showGeolocationInformations() structure
    Now this method creates an container for these data(newInfo) and gets
    the latitude and longitude from geolocation API -
    https://developer.mozilla.org/pl/docs/Web/API/Geolocation_API - based on
    latitude and lingitude there is create an unique api reference which
    will be passed to fetch()

--- src/modules/classes/Geocoding.js
+++ src/modules/classes/Geocoding.js
@@ -6,10 +6,38 @@ class Geocoding{
         this.infoContainer = infoContainer;
 
         //Call the method showing latitude/longitude
-        this.test();
+        this.showGeolocationInformations();
     }
-    test(){
-        console.log("1")
+    showGeolocationInformations(){
+        //Create an appropriate HTML element(in this element the information will be kept) 
+        const newInfo = document.createElement("li");
+        newInfo.className = "basicList__element informations__info";
+
+        this.infoContainer.appendChild(newInfo);
+
+        //Check if the browser supports the geolocation api
+        if(!navigator.geolocation) {
+            newInfo.innerHTML = "Geolocation is not supported by your browser";
+        }else{
+            newInfo.innerHTML = "Locating…";
+            //If everything is ok use getCurrentPosition function
+            navigator.geolocation.getCurrentPosition(success, error);
+        }
+
+        //auxiliary functions related with geolocation API(https://developer.mozilla.org/pl/docs/Web/API/Geolocation_API)- these are navigator.geolocation.getCurrentPosition callbacks
+        function success(position) {
+            //Precise values
+            let latitude  = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            //Based on api key, latitude and longitude create the appropriate address - this address in passed to fetch
+            const apiRef = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${API_KEY}`;
+            
+            console.log(apiRef)
+          }
+        
+          function error() {
+            newInfo.innerHTML = "Unable to retrieve your location";
+          }
     }
 }
 
